cf8b4bbd482709a7ca899939511d507b25bbc017
kamlesh-chandel <kamlesh@gkmit.co>
2026-03-24 01:19:16 +0530

    fds

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 7000;
 
 
 app.use(cors());
@@ -254,6 +255,6 @@ async function updateRow(id) {
 `);
 });
 
-app.listen(PORT, "127.0.0.1", () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
